4f58a7d972ce5a50f5b64a7bd9f39ab3c659d8fd
Maxime <auneau.maxime@gmail.com>
2020-10-07 11:08:31 +0200

    Ajout photo, plus modifs home + contact

--- code/script.js
+++ code/script.js
@@ -6,23 +6,28 @@ function validate(){
   var mail = document.getElementById("mail").value;
   var message = document.getElementById("message").value;
   var error = document.getElementById("error");
-  
-  error.style.padding = "10px";
+  var errorA = document.getElementById("error1");
+  var errorB = document.getElementById("error2");
+
 
     var text;
     if(name.length < 4){
       text = "Verifiez votre Nom !";
+      error.style.padding = "2px";
       error.innerHTML = text;
+
       return false;
     }
     if(mail.indexOf("@") == -1 || mail.length < 6){
       text = "Verifiez votre E-mail !";
-      error.innerHTML = text;
+      errorA.style.padding = "2px";
+      errorA.innerHTML = text;
       return false
     }
     if (message.length < 50) {
       text = "S'il vous plaît inscrivez plus que 50 caractères pour le message"; // message à changer
-      error.innerHTML = text;
+      errorB.style.padding = "2px";
+      errorB.innerHTML = text;
       return false;
     }
     confirm("Êtes vous sure de vouloir valider ce formulaire de contact ?");
